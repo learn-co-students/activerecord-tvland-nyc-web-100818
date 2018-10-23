ac799417c9aab9eb9536701398aa0db1034bbdc9
Rexman17 <a.stone1794@gmail.com>
2018-10-23 16:29:05 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
-end
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles # lists all of the characters that actor has
+    self.characters.map do |character|
+      # binding.pry
+      "#{character.name} - #{character.show.name}"
+
+      # "Khaleesi - Game of Thrones"
+    end
+  end
+
+end # end of Actor class
